48922e74107f962d703102a799e40a0eb8171954
James Birnie <jamesbirnie@jamess-mbp.home>
2017-10-16 21:17:59 +0100

    Initial commit of files created by lein new
    James

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to results-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/results_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns results-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [results-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
